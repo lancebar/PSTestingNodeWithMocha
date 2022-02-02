efe216a0528aa1cecc0c36005749384fb6f1f315
Lance Barisdale <lance.barisdale@gmail.com>
2022-02-02 07:10:31 -0800

    complete course

---
+++ app/models/parts.spec.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+describe('some tests', () =>{
+    it('should pass', () => {
+        let result = true;
+        expect(result).to.be.true;
+
+      ;
+    });
+
+    it('should fail', () => {
+        ;
+      });
+    
+})
--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
-console.log('tested');
+describe('some tests', () => {
+  it("should pass", () => {
+      //some code
+  });
+  it("should fail", () => {
+    //throw new Error('a failed test')
+});
+});
